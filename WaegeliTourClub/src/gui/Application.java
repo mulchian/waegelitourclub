f5f402570bb0baf00127bc1031d7830392194cd2
mulchian <jannik.wolf@live.de>
2017-01-13 09:54:21 +0100

    Update Application.java
    Überarbeitet

--- WaegeliTourClub/src/gui/Application.java
+++ WaegeliTourClub/src/gui/Application.java
@@ -1,18 +1,12 @@
 package gui;
 
-import javax.swing.JFrame;
-
 public class Application {
 
 	public static void main(String[] args) {
-		
-		//GUILogin gl = new GUILogin();
-		//gl.baueGUI();
-		
-		JFrame jFrame = new JFrame("Waegelitourclubverwaltung");
-		jFrame.setSize(200, 200);
-		jFrame.setVisible(true);
-
+		System.out.println("Startet!");
+		GUILogin gl = new GUILogin();
+		gl.baueGUI();
 	}
 
 }
+
