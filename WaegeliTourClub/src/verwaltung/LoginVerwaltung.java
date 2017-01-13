707383c43a189a7cd0c531a962110151b0ae8cbd
mulchian <jannik.wolf@live.de>
2017-01-13 09:55:37 +0100

    Update LoginVerwaltung.java
    Überarbeitet

--- WaegeliTourClub/src/verwaltung/LoginVerwaltung.java
+++ WaegeliTourClub/src/verwaltung/LoginVerwaltung.java
@@ -1,5 +1,42 @@
 package verwaltung;
 
+import speicher.Speicher;
+import klassen.*;
+
 public class LoginVerwaltung {
 
+    private Speicher speicher;
+    private String username;
+
+    public LoginVerwaltung(Speicher speicher, String username) {
+        this.speicher = speicher;
+        this.username = username;
+    }
+    
+    public LoginVerwaltung() {
+    }
+    
+    private boolean isAdmin() {
+        return false;
+    }
+    
+    private Mitglied convertUser(String pUsername) {
+        return null;
+    }
+    
+    private Admin convertAdmin(String pUsername) {
+        return null;
+    }
+    
+    public void login(String pUsername, String pPasswort) {
+        
+    }
+
+    public void openVerwaltung() {
+    
+    }
+    
+    public void openJFrame() {
+    
+    }
 }
